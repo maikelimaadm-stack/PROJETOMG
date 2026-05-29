22cb83f35ffee0c0c009581b3ae862d33321acde
maikelimaadm-stack <maike.lima.adm@gmail.com>
2026-05-29 17:36:36 -0400

    Delete src/components/movimentacoes directory

--- src/components/movimentacoes/movimentacaoOperacaoRules.jsx
+++
@@ -1,9 +0,0 @@
-export const OPERACOES_COM_FINANCEIRO = ['compra', 'bonificacao', 'venda'];
-
-export const OPERACOES_COM_DOCUMENTO = ['compra', 'bonificacao', 'venda', 'devolucao_cliente', 'devolucao_fornecedor'];
-
-export const OPERACOES_COM_FORNECEDOR = ['compra', 'bonificacao', 'devolucao_fornecedor'];
-
-export const OPERACOES_COM_CLIENTE = ['venda', 'devolucao_cliente'];
-
-export const OPERACOES_COM_MOTIVO = ['ajuste_positivo', 'ajuste_negativo', 'perda_quebra', 'consumo_interno', 'manutencao', 'aplicacao_area', 'outros_entrada', 'outros_saida'];
